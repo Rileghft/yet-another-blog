735d6ae49e178c4fa43ba21eb311fd13cc5bb415
Rileghft <mywonderemailyoung@gmail.com>
2018-09-20 12:39:11 +0800

    Finish password & email change feature

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,7 +3,7 @@
 from flask import render_template, redirect, url_for, flash, request, current_app
 from flask_login import current_user, login_required, login_user, logout_user
 from . import auth
-from .forms import LoginForm, RegisterForm
+from .forms import LoginForm, RegisterForm, EmailChangeForm, PasswordChangeForm
 from app import db
 from app.models import User
 from app.email import send_email
@@ -86,6 +86,47 @@ def resend_confirmation():
     else:
         flash('Your account is already confirmed.')
     return redirect(url_for('auth.unconfirm'))
+
+
+@auth.route('change_password', methods=['GET', 'POST'])
+@login_required
+def change_password():
+    form = PasswordChangeForm()
+    if form.validate_on_submit():
+        old_password = form.old_password.data
+        new_password = form.new_password.data
+        if current_user.reset_password(old_password, new_password):
+            flash('Change password successfully.')
+            db.session.add(current_user)
+            db.session.commit()
+            send_email([current_user.email], 'Password Changed', 'auth/mail/notify-password-change')
+        else:
+            flash('Failed to change password.')
+        return redirect(url_for('main.user_page', username=current_user.username))
+    return render_template('auth/change_password.html')
+
+
+@auth.route('change_email', methods=['GET', 'POST'])
+@login_required
+def change_email():
+    form = EmailChangeForm()
+    if form.validate_on_submit():
+        new_email = form.email.data
+        token = current_user.generate_email_confirmation_token(new_email)
+        send_email([new_email], 'Confirm Your Email Change', 'auth/mail/confirm-email-change', token=token, ts=datetime.now())
+        flash('Please check your email to confirm email address change.')
+        return redirect(url_for('main.user_page', username=current_user.username))
+    return render_template('auth/change_email.html')
+
+
+@auth.route('confirm_email_change/<token>')
+@login_required
+def confirm_email_change(token):
+    if current_user.confirm_email_change(token):
+        flash('Change email address successfully.')
+    else:
+        flash('Failed to change email address.')
+    return redirect(url_for('main.user_page', username=current_user.username))
 
 
 @auth.route('logout')
